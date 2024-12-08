65df1b4907e08d83dae78e84900a8bda709050db
fatimaimrannn <fatima91119@gmail.com>
2024-12-08 18:20:09 +0500

    Editing cd

--- backend/test_login.py
+++ backend/test_login.py
@@ -6,7 +6,7 @@ BASE_URL = "http://127.0.0.1:5000"
 def test_login():
     # Data for testing login
     login_data = {
-        "username": "testuser",
+        "username": "testuserr",
         "password": "securepassword"
     }
 
